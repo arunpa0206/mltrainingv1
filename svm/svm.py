06e55c18eae4b3996c6f8b13d0db53ff87556f78
Arun Panayappan <arun@Aruns-MacBook-Pro.local>
2018-08-24 15:58:24 +0530

    svm initial commit

---
+++ svm/svm.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn import preprocessing, model_selection as cross_validation, neighbors, svm
+import pandas as pd
+
+#Read data, clean data and drop unwanted features
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?',-99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+#Drop prediction class from data set, Add to y
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+#80-20 for train:test
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = svm.SVC()
+#Run SVM and calculate confidence
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
+
+example_measures = np.array([[4,2,1,1,1,2,3,2,1]])
+example_measures = example_measures.reshape(len(example_measures), -1)
+prediction = clf.predict(example_measures)
+print(prediction)
