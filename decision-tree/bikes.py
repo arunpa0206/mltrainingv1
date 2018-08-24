c233739d391ad9ba2a99c8e1a3b36c510361e7d2
Arun Panayappan <arun@Aruns-MacBook-Pro.local>
2018-08-24 16:21:57 +0530

    Decision Trees initial commit

---
+++ decision-tree/bikes.py
@@ -0,0 +1,32 @@
+import pandas as pd
+bikes = pd.read_csv('bikes.csv')
+print(bikes.head())
+
+#Plot the weather against bike count to see for relationship
+from matplotlib import pyplot as plt
+
+plt.figure(figsize=(8,6))
+plt.plot(bikes['temperature'], bikes['count'], 'o')
+plt.xlabel('temperature')
+plt.ylabel('bikes')
+plt.show()
+
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+
+regressor = DecisionTreeRegressor(max_depth=2)
+regressor.fit(np.array([bikes['temperature']]).T, bikes['count'])
+
+print(regressor.predict(5.))
+
+#You can visualise the prediction when temperature varies as follows:
+
+xx = np.array([np.linspace(-5, 40, 100)]).T
+
+plt.figure(figsize=(8,6))
+plt.plot(bikes['temperature'], bikes['count'], 'o', label='observation')
+plt.plot(xx, regressor.predict(xx), linewidth=4, alpha=.7, label='prediction')
+plt.xlabel('temperature')
+plt.ylabel('bikes')
+plt.legend()
+plt.show()
